fa8ef498d212809b27fe712e6dffa1dbcc6dd3db
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-07-24 17:15:57 -0400

    Started auth system

--- App.js
+++ App.js
@@ -1,2 +1,22 @@
-import App from './components/Index.js'
+import { SwitchNavigator } from 'react-navigation'
+
+// import the different screens
+import Main from './components/Index.js'
+import Loading from './components/authentication/Loading'
+import SignUp from './components/authentication/Signup'
+import Login from './components/authentication/Login'
+
+// create our app's navigation stack
+const App = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
+  }
+)
+
 export default App
--- components/Index.js
+++ components/Index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Platform, Image, View } from 'react-native'
 
 export default class Index extends React.Component {
+  state = { currentUser: null }
   render() {
+    const { currentUser } = this.state
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hi {currentUser && currentUser.email}!</Text>
       </View>
     )
   }
@@ -16,8 +16,7 @@ export default class Index extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
