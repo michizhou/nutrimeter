d4a9e7f41a983a85f484a70b67a6729a2151052d
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-09-29 23:42:50 -0400

    Added/Camera

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// Temporary FOR TESTING PURPOSES
+import Camera from '../Camera'
+
 class Profile extends React.Component {
   render() {
     return (
